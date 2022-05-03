c159e1c8cfda48cab8c160a68eef6fb441965e6a
JBLee <brianlee2795@gmail.com>
2022-05-03 11:43:25 -0700

    Bare Minimum Updates
    Some bare minimum updates for compatibility with personal setup. Need
    a script to copy over iterm plist settings to ~/Library/Preferences

--- antibody/install.sh
+++ antibody/install.sh
@@ -5,5 +5,6 @@ if command -v brew >/dev/null 2>&1; then
 else
 	curl -sL https://git.io/antibody | sudo sh -s -- -b /usr/local/bin
 fi
+export PATH="opt/homebrew/bin/:$PATH"
 antibody bundle <"$DOTFILES/antibody/bundles.txt" >~/.zsh_plugins.sh
 antibody update
